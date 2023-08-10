042cd342e9ea37cd01d9a30d47b81494f23420d5
Tom White <tom.e.white@gmail.com>
2023-08-10 14:41:24 +0100

    Add export to Excel

---
+++ export.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(writexl)
+
+# Export as Excel spreadsheets
+
+write_xlsx(
+  list(
+    "ages" = ages,
+    "households" = households,
+    "household sizes" = household_sizes,
+    "residents" = residents
+  ),
+  "data/results/categories/population.xls"
+)
+
+write_xlsx(
+  list(
+    "accommodation type" = accommodation_type,
+    "bedrooms" = bedrooms,
+    "tenure" = tenure,
+    "house prices" = house_prices
+  ),
+  "data/results/categories/housing.xls"
+)
+
+write_xlsx(
+  list(
+    "cars" = cars
+  ),
+  "data/results/categories/travel.xls"
+)
+
+write_xlsx(
+  list(
+    "economic activity" = economic_activity
+  ),
+  "data/results/categories/work.xls"
+)
